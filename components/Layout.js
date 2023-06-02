e2febda52548bbb0af6afe01a11a0e47b73cc696
Ruman-Islam <rumanislam48@gmail.com>
2023-06-02 14:18:16 +0600

    feat: favicon added

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({ title, children }) => {
         <title>{title ? title + " | ShopO" : "ShopO"}</title>
         <meta name="description" content="ShopO" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <div>
